192bc4430cfa8735c96ab4aba59e0e2af61b9f87
Olly Turner <46792585+ollyturner@users.noreply.github.com>
2023-07-13 10:42:59 +0200

    Added Songs

--- songData.js
+++ songData.js
@@ -1,4 +1,5 @@
 let songData = [
+    //1
     "Tim McGraw",
     "Fearless",
 
@@ -8,6 +9,88 @@ let songData = [
     "Teadrops On My Guitar",
     "Love Story",
 
-    "Fearless",
-    "Fearless",
+    "A Place In This World",
+    "Hey Stephen",
+
+    "Cold As You",
+    "White Horse",
+
+    "The Outside",
+    "You Belong With Me",
+
+    "Tied Together With A Smile",
+    "Breathe",
+
+    "Stay Beautiful",
+    "Tell Me Why",
+
+    "Should've Said No",
+    "You're Not Sorry",
+
+    "Mary's Song",
+    "The Way I Loved You",
+    
+    "Our Song",
+    "Forever & Always",
+
+    "I'm Only Me When I'm With You",
+    "The Best Day",
+    
+    "Invisible",
+    "Change",
+
+    "A Perfectly Good Heart",
+    "Jump Then Fall",
+
+    "Untouchable",
+
+    "Come In With The Rain",
+
+    "SuperStar",
+
+    "The Other Side Of The Door",
+
+    //2
+    "Mine",
+    "State of Grace",
+
+    "Sparks Fly",
+    "Red",
+
+    "Back To December",
+    "Treacherous",
+
+    "Speak Now",
+    "I Knew You Were Trouble",
+    
+    "Dear John",
+    "All Too Well",
+    
+    "Mean",
+    "22",
+
+    "The Story Of Us",
+    "I Almost Do",
+
+    "Never Grow Up",
+    "We Are Never Ever Getting Back Together",
+
+    "Enchanted",
+    "Stay Stay Stay",
+
+    "Better Than Revenge",
+    "The Last Time",
+
+    "Innocent",
+    "Holy Ground",
+
+    "Haunted",
+    "Sad Beautiful Tragic",
+    
+    "Last Kiss",
+    "The Lucky One",
+
+    "Long Live",
+    "Everything Has Changed",
 ]
+const total = songData.length;
